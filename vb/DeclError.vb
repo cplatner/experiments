1733558814e90b9a53e16e4b2115953632808880
cplatner <chris@platner.com>
2018-11-16 08:30:07 -0800

    - VB :(

---
+++ vb/DeclError.vb
@@ -0,0 +1,13 @@
+
+Module Module1
+ 
+    Sub Main()
+        Dim str As String = "Hello"
+        Dim intLength As Integer
+        intLength = Len(str)
+        Console.WriteLine(str)
+        Console.WriteLine(intLength)
+    End Sub
+ 
+End Module
+
---
+++ vb/Hello.vb
@@ -0,0 +1,9 @@
+
+Module Module1
+ 
+    Sub Main()
+        Console.WriteLine("Hello, world!")
+    End Sub
+ 
+End Module
+
